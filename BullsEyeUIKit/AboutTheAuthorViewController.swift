5c461772a61ad03022e5740f6e05a6e8ee2a969d
Idanthyrsus <mocker-waves@mail.ru>
2022-07-28 07:03:11 -0700

    Bull's Eye UIKit
    Enjoy old school game written using most common UIKit strategies

---
+++ BullsEyeUIKit/AboutTheAuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutTheAuthorViewController.swift
+//  BullsEyeUIKit
+//
+//  Created by Alexander Korchak on 27.07.2022.
+//
+
+import UIKit
+
+class AboutTheAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func back() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
